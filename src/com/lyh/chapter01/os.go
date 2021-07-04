7c23bb7c8beb363295aea4d139429ce1c4c2cd33
lvyanghui <312386021@qq.com>
2021-07-04 11:00:09 +0800

    feat: chapter one os practice

--- src/com/lyh/chapter01/os.go
+++ src/com/lyh/chapter01/os.go
@@ -34,3 +34,27 @@ func OsParam3(){
 
 	fmt.Println(strings.Join(os.Args[1:]," "))
 }
+
+func Practice1(){
+
+	fmt.Println(os.Args[0])
+	os.Args[0] = "Practice"
+	fmt.Println(os.Args[0])
+}
+
+func Practice2(){
+
+	for i := 1; i < len(os.Args); i++ {
+		fmt.Printf("i = %d,param= %s ", i , os.Args[i])
+		fmt.Println()
+	}
+}
+
+
+/**
+暂时不会用time
+ */
+func Practice3(){
+
+
+}
--- src/com/lyh/main.go
+++ src/com/lyh/main.go
@@ -12,4 +12,10 @@ func main(){
 	chapter01.OsParam2()
 
 	chapter01.OsParam3()
+
+	chapter01.Practice1()
+
+	chapter01.Practice2()
+
+	chapter01.Practice3()
 }
